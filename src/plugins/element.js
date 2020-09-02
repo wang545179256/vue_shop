06bd2f2e3a1dc2f1c17838342647116654acefd5
wang545179256 <545179256@qq.com>
2020-09-02 17:45:25 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,15 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {
+	Button,
+	Form,
+	FormItem,
+	Input,
+	Message
+} from 'element-ui'
+//导入弹框提示组件
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
